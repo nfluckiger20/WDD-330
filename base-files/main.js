2595c80e6b5e115b7db592f5797c6fdc829748d7
nfluckiger20 <flu20002@byui.edu>
2023-01-11 11:29:14 -0700

    final javascript

--- base-files/main.js
+++ base-files/main.js
@@ -7,101 +7,85 @@ When the page loads
         render the list of pokemon
         style the clicked type as active
         filter out types with no pokemon (shadow and unknown)
-
-
 */
-
 // we have to convert the response fetch sends back into the appropriate type. In this case the API sends back json...so we process it as json and send it back
-function convertToJson(response){
+function convertToJson(response) {
     if(response.ok){
         return response.json();
     }
-        else{
-            throw new Error("bad response");
-        }
+    else{
+        throw new Error("bad response");
+    }
 }
-
 // once we have the data, we need to do something with it. This won't always be the same thing though. With a callback (function passed into a function) we can change the behavior as needed.
-function getData(url,callback){
-    fetch(url) 
+async function getData(url, callback) {
+    // const data = await fetch(url);
+    // const json = await convertToJson(data);
+    // // console.log(json);
+    // if(callback) {
+    //     callback(json);
+    // }
+    fetch(url)
         .then(convertToJson)
         .then((data) => {
             console.log(data);
-            //notice : this is where we execute the function that passed in
-            if (callback){
+            if(callback){
                 callback(data);
             }
-        }
-        )
+        })
 }
-//getData("https://pokeapi.co/api/v2/type");
-
-// keep things from happening until the DOM is ready
-// another alternative would be to add 'defer' to our script element
-window.addEventListener("load",function (){
-    //get and display the list of types
+window.addEventListener("load", function() {
     getData("https://pokeapi.co/api/v2/type", renderTypeList);
-    
-    document.getElementById("typeList").addEventListener("click",typeClickedHandler);
-})
-
+    document.getElementById("typeList").addEventListener("click", typeClickedHandler)
+});
 // create simple html markup to display a list
-function renderTypeList(list){
+function renderTypeList(list) {
     const element = document.getElementById("typeList");
     const cleanList = cleanTypeList(list.results);
-    cleanList.forEach((item)=>{
-
-        const li=document.createElement("li");
-        li.innerHTML = '${item.name}';
-
-        li.setAttribute("data-url",item.url);
+    cleanList.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerHTML = `${item.name}`;
+        li.setAttribute("data-url", item.url);      // data-url stores additional information for developers to utilize
         element.appendChild(li);
     })
 }
-
-function typeClickedHandler (event){
-    //note difference between 2 consoles below
-    console.log(event.target);
-    console.log(event.currentTarget);
-
-    const selectedType = event.target;
-    const url = selectedType.dataset.url;
-
+// create click handler
+function typeClickedHandler(event) {
+    // console.log(event.target);
+    // console.log(event.currentTarget);  // a more generic higher level target 
+    const selectType = event.target;
+    const url = selectType.dataset.url;
+    getData(url, renderPokeList);
     setActive(url);
-
-    getData(url,renderPokeList);
 }
-
-function renderPokeList(list){
+// render the list
+function renderPokeList(list) {
     const element = document.getElementById("pokeList");
     element.innerHTML = "";
-    list.pokeman.forEach((item)=> {
-        const li = documnet.createElement("li");
-        li.innerHTML = '${item.pokemon.name}';
-        li.setAttribute("data-url", item.pokeman.url);
+    const header = document.createElement("h2");
+    header.innerHTML = `${list.name} (${list.pokemon.length})`;
+    element.appendChild(header);
+    list.pokemon.forEach((item)=>{
+        const li = document.createElement("li");
+        li.innerHTML = `${item.pokemon.name}`;
+        li.setAttribute("data-url", item.pokemon.url);
         element.appendChild(li);
     })
+    // use a for each loop to print out the pokemon in each type below.
 }
-
-function setActive(type){
-    const allTypes = document.querySelectorAll(".types>li");
-    allTypes.forEach((item) =>{
-        if(item.dataset.url === type){
-            item.classList.add("active");
+// set active item
+function setActive(type) {
+    const allTypes = document.querySelectorAll(".types > li"); // > in query represents direct descendent children
+    allTypes.forEach((item)=>{
+        if(item.dataset.url == type){
+            item.classList.add("active"); // adds a class active to each class that meets the boolean and is associated with the items.
         }
-        else{
+        else {
             item.classList.remove("active");
         }
     })
 }
-// create click handler
-
-// render the list
-
-// set active item
-
 // there are no pokemon in unknown and shadow. Let's remove them from the list.
-
-function cleanTypeList(list){
-    return list.filter((item)=>item.name != "shadow" && item.name != "unknown");
+function cleanTypeList(list) {
+    return list.filter((item)=> item.name != "shadow" && item.name != "unknown"); // if true leave in if false filter out
 }
